20f2f4268f7398239b763f20a6bc6887bfa4d0a4
Optimist <optimist@sdf-eu.org>
2015-07-03 20:44:06 +0100

    Some simple coloring.

--- chapter01/triangle.js
+++ chapter01/triangle.js
@@ -15,7 +15,7 @@ window.onload = function init()
 function setup(canvas , gl)
 {
     gl.viewport(0 , 0 , canvas.width , canvas.height);
-    gl.clearColor(0.0 , 0.0 , 1.0 , 1.0);
+    gl.clearColor(1.0 , 1.0 , 1.0 , 1.0);
 
     var program = init_program(gl , "vertex-shader" , "fragment-shader");
     gl.useProgram(program);
@@ -23,6 +23,10 @@ function setup(canvas , gl)
     var vertices = create_polygon(sides);
     bind_buffer(gl , vertices);
     bind_attribute(gl, program , "v_position" , 2);
+
+    var colors = create_colors(sides);
+    bind_buffer(gl , colors);
+    bind_attribute(gl , program , "v_color" , 3);
 }
 
 function render(canvas , gl)
@@ -45,4 +49,19 @@ function create_polygon(faces)
 
     var polygon = new Float32Array(nodes);
     return polygon;
+}
+
+function create_colors(faces)
+{
+    var colors = [];
+    var step = 1.0 / faces;
+    for(var i = 0 ; i < faces ; i++)
+    {
+	var i_step = i * step;
+	colors.push(i_step);
+	colors.push(i_step);
+	colors.push(i_step);
+    }
+    colors = new Float32Array(colors);
+    return colors;
 }
