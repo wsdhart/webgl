766cd728ce21dff70126bed018d0ccf06e7bb889
Optimist <optimist@sdf-eu.org>
2015-07-06 07:13:15 +0100

    Chapter 2 continues on from Chapter 1, with a function for rotating points.
    This example starts off with a triangle made up of 3 triangles to illustrate
    rotation, that is to say that rotations are more obvious with the obvious
    exceptions of 2/3π and 4/3π.
    
    This commit includes another of John Tenniel's illustrations from Alice's
    Adventures in Wonderland.

---
+++ chapter02/triangle.js
@@ -0,0 +1,70 @@
+"use strict";
+
+var sides = 3;
+var subdivisions = 3;
+var holes = true;
+
+window.onload = function init()
+{
+    var canvas = document.getElementById("webgl-canvas");
+    var gl = WebGLUtils.setupWebGL(canvas);
+    if(!gl){ alert("Failed initialising WebGL"); }
+
+    var vertices = setup(canvas , gl);
+    render(gl , vertices);
+}
+
+function setup(canvas , gl)
+{
+    gl.viewport(0 , 0 , canvas.width , canvas.height);
+    gl.clearColor(1.0 , 1.0 , 1.0 , 1.0);
+
+    var program = init_program(gl , "vertex-shader" , "fragment-shader");
+    gl.useProgram(program);
+
+    var vertices = create_polygon(sides);
+    for(var i = 0 ; i < subdivisions ; i++)
+	vertices = split_vertices(vertices , holes);
+    vertices = rotate_points(vertices , Math.PI / 2);
+    bind_buffer(gl , vertices);
+    bind_attribute(gl, program , "v_position" , 2);
+
+    var colors = create_colors(vertices.length / 6);
+    bind_buffer(gl , colors);
+    bind_attribute(gl , program , "v_color" , 3);
+
+    return vertices;
+}
+
+function render(gl , vertices)
+{
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.drawArrays(gl.TRIANGLES , 0 , vertices.length / 2);
+}
+
+function create_colors(faces)
+{
+    var colors = [];
+    var step = 1.0 / faces;
+    for(var i = 0 ; i < faces ; i++)
+    {
+	var i_step = i * step;
+	for(var j = 0 ; j < 9 ; j++)
+	    colors.push(i_step);
+    }
+    return colors;
+}
+
+function rotate_points(vertices , angle)
+{
+    for(var i = 0 ; i < vertices.length ; i+=2)
+    {
+	var x = vertices[i];
+	var y = vertices[i + 1];
+	var xp = (x * Math.sin(angle)) + (y * Math.cos(angle));
+	var yp = (x * (Math.cos(angle)) - (y * Math.sin(angle)));
+	vertices[i] = xp;
+	vertices[i + 1] = yp;
+    }
+    return vertices;
+}
