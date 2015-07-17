wsdhart/webgl

commit 456bf8703eba7b15d51124981939c5d8aacd7d4c

Reuse bound attributes.
Following on from commit 467bf5e5868e5ea1a0123

chapter03/triangle.js
@@ -6,6 +6,8 @@ var program;
 var vertex_buffer;
 var color_buffer;
 var colors;
+var v_color;
+var v_position;
 
 var sides = 3;
 var subdivisions = 0;
@@ -52,9 +54,9 @@ function render(vertices)
     if(!vertices)
 	vertices = polygon;
     vertex_buffer = bind_buffer(gl , vertices);
-    bind_attribute(gl, program , "v_position" , 2);
+    v_position = bind_attribute(gl, program , "v_position" , 2 , v_position);
 
-    color_buffer = bind_buffer(gl , colors , color_buffer);
+    v_color = color_buffer = bind_buffer(gl , colors , color_buffer , v_color);
     bind_attribute(gl , program , "v_color" , 3);
 
     gl.uniform1i(u_option , option);

commit ab13c5fd7406b2f8b272fd8fbf3357bc30af07f1

Correct bug introduced in commit 456bf8703eba
The previously committed patch is alright until the last line where I seem to have slept right through. I had scrolled past it twice before something looked funky about it. The assignment is due tomorrow.

chapter03/triangle.js
@@ -55,15 +55,15 @@ function setup()
 function bind_colors()
 {
     colors = create_rgb(polygon.length / 6);
-    v_color = color_buffer = bind_buffer(gl , colors , color_buffer , v_color);
-    bind_attribute(gl , program , "v_color" , 3);
+    color_buffer = bind_buffer(gl , colors , color_buffer);
+    v_color = bind_attribute(gl , program , "v_color" , 3 , v_color);
 }
 
 function bind_vertices(vertices)
 {
     if(!vertices)
 	vertices = polygon;
-    vertex_buffer = bind_buffer(gl , vertices);
+    vertex_buffer = bind_buffer(gl , vertices , vertex_buffer);
     v_position = bind_attribute(gl, program , "v_position" , 2 , v_position);
 }
 