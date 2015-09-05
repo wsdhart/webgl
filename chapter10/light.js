8aae98ef9bb1a03f09883fcff20c451b780dd239
Optimist <optimist@sdf-eu.org>
2015-09-05 16:57:53 +0100

    Nicer defaults.

--- chapter10/light.js
+++ chapter10/light.js
@@ -5,7 +5,7 @@ function Light(gl , program)
     this.gl = gl;
     this.program = program;
 
-    this.position = [0 , 2 , -3 , 0];
+    this.position = [0 , 2 , -5 , 0];
 
     this.ambient = [1 , 1 , 1 , 1];
     this.diffuse = [1 , 1 , 1 , 1];
